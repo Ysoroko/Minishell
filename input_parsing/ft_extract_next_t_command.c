c2a8f16a83cad201a45964c609d255d2f221a090
ysoroko <ysoroko@student.s19.be>
2021-07-26 16:23:54 +0200

    now empty spaces coming from empty quotes and env vars are ignored

--- input_parsing/ft_extract_next_t_command.c
+++ input_parsing/ft_extract_next_t_command.c
@@ -6,13 +6,13 @@
 /*   By: ysoroko <ysoroko@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/30 15:52:06 by ysoroko           #+#    #+#             */
-/*   Updated: 2021/07/26 15:31:43 by ysoroko          ###   ########.fr       */
+/*   Updated: 2021/07/26 16:22:59 by ysoroko          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/minishell.h"
 
-static void	ft_quotes_env_variables_and_update_macros(t_command *command)
+static char	**ft_quotes_env_variables_and_update_macros(t_command *command)
 {
 	char	**str_tab_all;
 	int		*macros_tab;
@@ -38,7 +38,58 @@ static void	ft_quotes_env_variables_and_update_macros(t_command *command)
 			}
 		}
 	}
-	command->str_tab_all = (char **)ft_free_str_tab(&str_tab_all, temp);
+	;
+	return ((char **)(ft_free_str_tab(&str_tab_all, temp)));
+}
+
+/*
+** int	ft_str_tab_len_without_str_with_only_excl(char **s_tab, char *excl)
+** This function is used to count the length of str_tab needed to malloc.
+** It doesn't count the words in s_tab composed solely of characters
+** present in *excl argument.
+** In minishell it's used to avoid counting strings composed only of spaces
+*/
+
+static int	ft_str_tab_len_without_str_with_only_excl(char **s_tab, char *excl)
+{
+	int	i;
+	int	count;
+
+	i = -1;
+	count = 0;
+	while (s_tab[++i])
+	{
+		if (!ft_str_only_has_chars_from_charset(s_tab[i], excl))
+			count++;
+	}
+	return (count);
+}
+
+/*
+** char **ft_copy_str_tab_except_for(char **str_tab, char *except)
+** This function is used to copy **str_tab but not copy
+** strings composed only of characters present in *except argument
+** In minishell, this is used to avoid copying strings composed
+** only of spaces.
+*/
+
+static char	**ft_copy_str_tab_except_for(char **str_tab, char *except)
+{
+	char	**ret;
+	int		len;
+	int		i;
+	int		j;
+
+	len = ft_str_tab_len_without_str_with_only_excl(str_tab, except);
+	ret = ft_calloc_exit(len + 1, sizeof(*str_tab));
+	i = -1;
+	j = -1;
+	while (str_tab[++i])
+	{
+		if (!ft_str_only_has_chars_from_charset(str_tab[i], except))
+			ret[++j] = ft_strdup_exit(str_tab[i]);
+	}
+	return (ret);
 }
 
 /*
@@ -55,14 +106,17 @@ t_command	*ft_extract_next_t_command(char *input_checkpt, int *i)
 	int			j;
 	t_command	*command;
 	char		*next_command_as_str;
+	char		**temp;
 
 	command = ft_calloc_exit(1, sizeof(t_command));
 	next_command_as_str = ft_extract_next_command_string(input_checkpt);
 	j = ft_strlen(next_command_as_str);
 	ft_extract_str_tab_all(next_command_as_str, command);
 	ft_extract_role_macros_tab(command);
-	ft_quotes_env_variables_and_update_macros(command);
+	temp = ft_quotes_env_variables_and_update_macros(command);
+	command->str_tab_all = ft_copy_str_tab_except_for(temp, SPACES);
 	ft_extract_str_tab_for_execve(command);
+	ft_free_str_tab(&temp, 0);
 	ft_free_str(&next_command_as_str);
 	if (!j)
 		*i += 1;

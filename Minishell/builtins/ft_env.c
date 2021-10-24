32305381018a70da1ee6bb02fff6516b0f06a3d0
ysoroko <ysoroko@student.s19.be>
2021-10-24 16:16:07 +0200

    DS Stores removed, ft_env.c adapted for error checking

--- Minishell/builtins/ft_env.c
+++ Minishell/builtins/ft_env.c
@@ -6,7 +6,7 @@
 /*   By: ysoroko <ysoroko@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/29 16:16:10 by ablondel          #+#    #+#             */
-/*   Updated: 2021/10/24 15:54:28 by ysoroko          ###   ########.fr       */
+/*   Updated: 2021/10/24 16:15:48 by ysoroko          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,9 +27,13 @@ int	main(int ac, char **av, char **env)
 {
 	int	i;
 
-	(void)ac;
-	(void)av;
 	i = 0;
+	(void)av;
+	if (ac > 1)
+	{
+		ft_minishell_error("\"env\" only accepted without arguments/options");
+		exit(EXIT_FAILURE);
+	}
 	while (env[i])
 	{
 		if (ft_check_equal_sign(env[i]))
